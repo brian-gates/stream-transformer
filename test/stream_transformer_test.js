626ee186d636ddec59274bf29a6a21abb04135d4
Brian Gates <brian.g.gates@gmail.com>
2014-01-29 11:09:13 -0800

    Better test naming

--- test/stream_transformer_test.js
+++ test/stream_transformer_test.js
@@ -4,57 +4,57 @@ var StreamTransformer = require('../index');
 describe('StreamTransformer', function () {
 
   it('should work', function (done){
-    var ds = new StreamTransformer();
-    ds.write({foo: 'bar'});
-    ds.on('data', function (transformed) {
+    var transformer = new StreamTransformer();
+    transformer.write({foo: 'bar'});
+    transformer.on('data', function (transformed) {
       transformed.should.eql({foo: 'bar'});
     });
-    ds.on('end', done);
+    transformer.on('end', done);
   });
 
   it('should allow registering global tranformers', function (done){
-    var ds = new StreamTransformer();
-    ds.register(function (data, done){
+    var transformer = new StreamTransformer();
+    transformer.register(function (data, done){
       data.foo += '!';
       done(data);
     });
-    ds.write({foo: 'bar'});
-    ds.on('data', function (transformed){
+    transformer.write({foo: 'bar'});
+    transformer.on('data', function (transformed){
       transformed.should.eql({foo: 'bar!'});
     });
-    ds.on('end', done);
+    transformer.on('end', done);
   });
 
   it('should allow registering keyed tranformers', function (done){
-    var ds = new StreamTransformer();
-    ds.register('foo', function (data, done){
+    var transformer = new StreamTransformer();
+    transformer.register('foo', function (data, done){
       data.foo += '!';
       done(data);
     });
-    ds.write(['foo', {foo: 'bar'}]);
-    ds.on('data', function (transformed){
+    transformer.write(['foo', {foo: 'bar'}]);
+    transformer.on('data', function (transformed){
       transformed.should.eql({foo: 'bar!'});
     });
-    ds.on('end', done);
+    transformer.on('end', done);
   });
 
   it('should allow calling super', function (done){
-    var ds = new StreamTransformer();
-    ds.register('foo', function (data, done){
+    var transformer = new StreamTransformer();
+    transformer.register('foo', function (data, done){
       data.foo += '!';
       done(data);
     });
-    ds.register('foo', function (data, done){
+    transformer.register('foo', function (data, done){
       this._super(data, function (data){
         data.foo += '1';
         done(data);
       })
     });
-    ds.write(['foo', {foo: 'bar'}]);
-    ds.on('data', function (transformed){
+    transformer.write(['foo', {foo: 'bar'}]);
+    transformer.on('data', function (transformed){
       transformed.should.eql({foo: 'bar!1'});
     });
-    ds.on('end', done);
+    transformer.on('end', done);
   });
 
 });
